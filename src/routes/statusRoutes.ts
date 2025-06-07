c2bda0ec151802febae4db286e2992a147ad16bc
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-06-07 10:40:36 -0300

    feat(status): enhance status endpoint to return database metrics
    - Updated the status route to provide detailed database information including server version, max
    connections, and opened connections.

--- src/routes/statusRoutes.ts
+++ src/routes/statusRoutes.ts
@@ -1,9 +1,40 @@
 import express from "express";
+import { PrismaClient } from "../generated/prisma";
 
 const statusRouter = express.Router();
+const prisma = new PrismaClient();
 
-statusRouter.get("/api/v1/status", (_req, res) => {
-  res.sendStatus(200);
+statusRouter.get("/api/v1/status", async (_req, res) => {
+  const updatedAt = new Date().toISOString();
+
+  const databaseServerVersionResult = await prisma.$queryRaw<
+    { server_version: string }[]
+  >`SHOW server_version;`;
+  const databaseServerVersionValue =
+    databaseServerVersionResult[0].server_version;
+
+  const databaseMaxConnectionsResult = await prisma.$queryRaw<
+    { max_connections: string }[]
+  >`SHOW max_connections;`;
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await prisma.$queryRaw<
+    { count: number }[]
+  >`SELECT count(*) as count FROM pg_stat_activity;`;
+  const databaseOpenedConnectionsValue = Number(
+    databaseOpenedConnectionsResult[0].count,
+  );
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseServerVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 });
 
 export default statusRouter;
